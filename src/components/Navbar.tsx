be01058bcd54b5208bf10e5b4711ffc66fc6e0d2
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-22 07:35:53 -0600

    gave nav a static height for better display on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,13 @@ import DarkModeToggle from "./DarkModeToggle"
 const Navbar = async () => {
     const session = await getAuthSession();
     return (
-        <div className="fixed top-0 inset-x-0 h-fit bg-background border-b border-border z-[10] py-3">
+        <div className="fixed top-0 inset-x-0 h-18 bg-background border-b border-border z-[10] py-3">
             <div className='max-w-7xl h-full mx-auto flex items-center justify-between px-7 md:px-12 gap-3'>
                 <Link href='/' className='flex items-center'>
                     <Icons.logo className='h-8 w-8 sm:h-9 sm:w-9 translate-y-0.5' />
                     <p className='hidden text-foreground text-2xl font-extrabold md:block'>Voxer</p>
                 </Link>
-                {<SearchBar />}
+                <SearchBar />
                 <div className="flex justify-center items-center gap-4">
                     <DarkModeToggle className="hidden md:block" />
                     {
